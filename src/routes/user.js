c5b0cb5804236e5799b29e3815fe232ad943203f
Sneha Paul <spaul1204@gmail.com>
2025-04-30 14:30:59 +0530

    added user/feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userAuth = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 const USER_DISPLAY_DATA = "firstName lastName age gender skills";
@@ -46,6 +47,37 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
     res.status(200).json({ data });
   } catch (e) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    //get connections from userConnection db
+    //check if profile fetched is not logged in user
+    //check if profile is already not a connection
+    //check if profile being fetched has not been ignored or rejected or interested by loggedin user
+    //check if profile being fetched has not ignored or rejected or interested by logged in user
+    const loggedInUser = req.user;
+    //all requests sent and recieved
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser.id }, { toUserId: loggedInUser.id }],
+    });
+    
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((each) => {
+      hideUsersFromFeed.add(each.fromUserId.toString());
+      hideUsersFromFeed.add(each.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser.id } },
+      ],
+    }).select(USER_DISPLAY_DATA);
+    res.status(200).json({ users });
+  } catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
